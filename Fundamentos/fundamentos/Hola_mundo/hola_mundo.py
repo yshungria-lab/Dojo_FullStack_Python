3e052888c9003537ddbfcb6a5e0443d993a30b0b
Yojan Stiben Hungria <yojanstibenh@gmail.com>
2022-11-15 22:21:00 -0500

    Primer Hola mundo en Python

---
+++ Fundamentos/fundamentos/Hola_mundo/hola_mundo.py
@@ -0,0 +1,11 @@
+print("hola mundo")
+nombre = "Yojan"
+print("Hola", nombre)
+numero_favorito = 7
+print("Hola Favorito", numero_favorito)
+
+Comida_favorita_1 = "Pizza"
+Comida_favorita_2 = "Tacos"
+print("Amo comer", Comida_favorita_1, "y", Comida_favorita_2)
+
+print("La comida favorita 1 tiene {} letras".format(len(Comida_favorita_1)))
