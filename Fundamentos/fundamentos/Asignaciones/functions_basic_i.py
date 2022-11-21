f6f3d85a62e0a42905ee30e7e52816270b85dc73
Yojan Stiben Hungria <yojanstibenh@gmail.com>
2022-11-20 19:36:59 -0500

    functions_basi_i terminado

--- Fundamentos/fundamentos/Asignaciones/functions_basic_i.py
+++ Fundamentos/fundamentos/Asignaciones/functions_basic_i.py
@@ -2,46 +2,49 @@
 def number_of_food_groups():
     return 5
 print(number_of_food_groups())
+# Solution: 5
+
 
 
 #2
 def number_of_military_branches():
     return 5
 print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-
+# Soluction: variable number_of_days_in_a_week_silicon_or_triangle_sides is not defined.
 
 #3
 def number_of_books_on_hold():
     return 5
     return 10
 print(number_of_books_on_hold())
-
+# solution: 5
 
 #4
 def number_of_fingers():
     return 5
     print(10)
 print(number_of_fingers())
-
+# solution: 5
 
 #5
 def number_of_great_lakes():
     print(5)
 x = number_of_great_lakes()
 print(x)
+# solution: 5 and none
 
 
 #6
 def add(b,c):
     print(b+c)
 print(add(1,2) + add(2,3))
-
+# solution: error, TypeError: unsupported operand type(s) for +: 'NoneType' and 'NoneType'
 
 #7
 def concatenate(b,c):
     return str(b)+str(c)
 print(concatenate(2,5))
-
+# solution: ´25´
 
 #8
 def number_of_oceans_or_fingers_or_continents():
@@ -53,6 +56,7 @@ def number_of_oceans_or_fingers_or_continents():
         return 10
     return 7
 print(number_of_oceans_or_fingers_or_continents())
+# solution: 100 - 10
 
 
 #9
@@ -65,13 +69,14 @@ def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
 print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-
+# solution: 7 and 14 and 21
 
 #10
 def addition(b,c):
     return b+c
     return 10
 print(addition(3,5))
+# solution:8
 
 
 #11
@@ -83,6 +88,7 @@ def foobar():
 print(b)
 foobar()
 print(b)
+# solution: 500 and 500 and 300 and 500.
 
 
 #12
@@ -95,7 +101,7 @@ def foobar():
 print(b)
 foobar()
 print(b)
-
+# solution: 500 and 500 and 300 and 500.
 
 #13
 b = 500
@@ -107,7 +113,7 @@ def foobar():
 print(b)
 b=foobar()
 print(b)
-
+# solution: 500 and 500 and 300 and 300.
 
 #14
 def foo():
@@ -117,6 +123,7 @@ def foo():
 def bar():
     print(3)
 foo()
+# solution: 1 and 3 and 2.
 
 
 #15
@@ -130,3 +137,5 @@ def bar():
     return 5
 y = foo()
 print(y)
+
+# solution: 1 and 3 and 5 and 10.
--- Fundamentos/fundamentos/Asignaciones/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-input_lowNum = int(input("Ingresa el numero menor: "))
-input_highNum = int(input("Ingresa el numero mayor: "))
-input_mult = int(input("Ingresa el numero multiplo: "))
-for x in range(input_lowNum, input_highNum+1, 1):
-    if x % input_mult == 0:
-        print(x)
